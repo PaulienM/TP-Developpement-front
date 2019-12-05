3743d386611e799947bc549e2e20efd6528193ed
Paulien <paulien.m.73@gmail.com>
2019-12-05 15:10:09 +0100

    create delete button in destination table

--- tp3/partie3/js/travel-table.js
+++ tp3/partie3/js/travel-table.js
@@ -5,18 +5,85 @@ const destinations = [
     {country: "Japon", circuit: "Coupe du monde", image: "image/japon.jpg", price: 40000}
 ];
 
+const tableTr = `
+<tr>
+    <th>Pays</th>
+    <th>Circuit</th>
+    <th>Image</th>
+    <th>Prix</th>
+    <th>Réservation</th>
+</tr>
+`;
+
+const form = `
+<form>
+    <div>
+        <label for="country">Pays</label>
+        <input type="text" id="country">
+    </div>
+    <div>
+        <label for="country">Pays</label>
+        <input type="text" id="country">
+    </div>
+    <div>
+        <label for="country">Pays</label>
+        <input type="text" id="country">
+    </div>
+    <div>
+        <label for="country">Pays</label>
+        <input type="text" id="country">
+    </div>
+</form>
+`;
+
 function displayDestinations() {
-    console.log("la");
-    destinations.forEach(element => {
+    let tableContent = document.getElementById("destination-tables").innerHTML = tableTr;
+    destinations.forEach((element, index) => {
         document.getElementById("destination-tables").innerHTML +=
             `<tr>
                 <td>${element.country}</td>
                 <td>${element.circuit}</td>
                 <td><img src="${element.image}" alt=""></td>
                 <td>${element.price}€</td>
-                <td><button>Réserver</button></td>
+                <td>
+                    <button>Réserver</button>
+                    <button class="remove-button" value="${index}">Supprimer</button>
+                    <button class="modify-button" value="${index}">Modifier</button>
+                </td>
             </tr>`
     });
 }
 
-window.addEventListener("load", displayDestinations);
+function createDestination(destination) {
+    destinations.push(destination);
+    displayDestinations();
+}
+
+function modifyDestination(destination, index) {
+    destinations[index] = destination;
+    displayDestinations();
+}
+
+function removeDestination(event) {
+    index = event.toElement.value;
+    destinations.splice(index, 1);
+    displayDestinations();
+    addEvent("remove-button", removeDestination);
+}
+
+function addEvent(elementClass, action) {
+    let button = document.getElementsByClassName(elementClass);
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener("click", action)
+    }
+}
+
+function displayFormToModify() {
+
+}
+
+window.addEventListener("load", function () {
+    displayDestinations();
+    addEvent("remove-button", removeDestination);
+    addEvent("modify-button", modifyEvent);
+});
