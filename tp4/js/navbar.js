3664d6a8b668355bca17e4be852bd3ba006fd610
Paulien <paulien.m.73@gmail.com>
2019-12-06 11:45:31 +0100

    Import bootstrap in all html files

--- tp4/js/navbar.js
+++ tp4/js/navbar.js
@@ -8,25 +8,32 @@ const navbar = `
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav row justify-content-around w-100">
-      <li class="nav-item ${fileName === "index.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "index.html" ? "active" : ""}">
         <a class="nav-link" href="index.html">Acceuil</a>
       </li>
-      <li class="nav-item ${fileName === "menu.html" ? "active" : false}">
-        <a class="nav-link" href="menu.html">Features</a>
+      <li class="nav-item dropdown ${fileName === "menu.html" ? "active" : ""}">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="menu.html">connexion</a>
+          <a class="dropdown-item" href="menu.html">Recherche</a>
+          <a class="dropdown-item" href="menu.html">Reservation</a>
+        </div>
       </li>
-      <li class="nav-item ${fileName === "services.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "services.html" ? "active" : ""}">
         <a class="nav-link" href="services.html">Services</a>
       </li>
-      <li class="nav-item ${fileName === "destinations.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "destinations.html" ? "active" : ""}">
         <a class="nav-link" href="destinations.html">Destinations</a>
       </li>
-      <li class="nav-item ${fileName === "audio.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "audio.html" ? "active" : ""}">
         <a class="nav-link" href="audio.html">Voyage virtuel : audio</a>
       </li>
-      <li class="nav-item ${fileName === "video.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "video.html" ? "active" : ""}">
         <a class="nav-link" href="video.html">Voyage virtuel : vidéo</a>
       </li>
-      <li class="nav-item ${fileName === "contact.html" ? "active" : false}">
+      <li class="nav-item ${fileName === "contact.html" ? "active" : ""}">
         <a class="nav-link" href="contact.html">Contact</a>
       </li>
     </ul>
