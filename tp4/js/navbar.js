d86b4887aa116928722805a6425e6072e636e657
Paulien <paulien.m.73@gmail.com>
2019-12-06 10:36:25 +0100

    Navbar with bootstrap

--- tp4/js/navbar.js
+++ tp4/js/navbar.js
@@ -1,24 +1,39 @@
 let locationArr = document.location.pathname.split("/");
 let fileName = locationArr[locationArr.length - 1];
 const navbar = `
-    <ul class="no-list-style menu-list">
-        <li><a href="index.html" ${fileName === "index.html" ? 'class="active"' : ""}>Acceuil</a></li>
-        <li>
-            <a id="menu" href="menu.html" ${fileName === "menu.html" ? 'class="active"' : ""}>Menu</a>
-            <ul class="no-list-style dropdown">
-                <li><a href="menu.html">connexion</a></li>
-                <li><a href="menu.html">Recherche</a></li>
-                <li><a href="menu.html">Reservation</a></li>
-            </ul>
-        </li>
-        <li><a href="services.html" ${fileName === "services.html" ? 'class="active"' : ""}>Services</a></li>
-        <li><a href="destinations.html" ${fileName === "destinations.html" ? 'class="active"' : ""}>Destinations</a></li>
-        <li><a href="audio.html" ${fileName === "audio.html" ? 'class="active"' : ""}>Voyage virtuel : audio</a></li>
-        <li><a href="video.html" ${fileName === "video.html" ? 'class="active"' : ""}>Voyage virtuel : vidéo</a></li>
-        <li><a href="contact.html" ${fileName === "contact.html" ? 'class="active"' : ""}>Contact</a></li>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav row justify-content-around w-100">
+      <li class="nav-item ${fileName === "index.html" ? "active" : false}">
+        <a class="nav-link" href="index.html">Acceuil</a>
+      </li>
+      <li class="nav-item ${fileName === "menu.html" ? "active" : false}">
+        <a class="nav-link" href="menu.html">Features</a>
+      </li>
+      <li class="nav-item ${fileName === "services.html" ? "active" : false}">
+        <a class="nav-link" href="services.html">Services</a>
+      </li>
+      <li class="nav-item ${fileName === "destinations.html" ? "active" : false}">
+        <a class="nav-link" href="destinations.html">Destinations</a>
+      </li>
+      <li class="nav-item ${fileName === "audio.html" ? "active" : false}">
+        <a class="nav-link" href="audio.html">Voyage virtuel : audio</a>
+      </li>
+      <li class="nav-item ${fileName === "video.html" ? "active" : false}">
+        <a class="nav-link" href="video.html">Voyage virtuel : vidéo</a>
+      </li>
+      <li class="nav-item ${fileName === "contact.html" ? "active" : false}">
+        <a class="nav-link" href="contact.html">Contact</a>
+      </li>
     </ul>
+  </div>
+</nav>
 `;
 
-window.addEventListener("load", function() {
-   document.getElementById("navbar").innerHTML = navbar;
+window.addEventListener("load", function () {
+    document.getElementById("navbar").innerHTML = navbar;
 });
